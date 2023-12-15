9857eece107cdca721042cd3dab75115b71a65c8
MYRADhub <myradism@gmail.com>
2023-12-14 20:56:24 -0500

    Fixed search suggestions

--- app/components/Search.js
+++ app/components/Search.js
@@ -2,39 +2,39 @@ import React, { useState } from 'react';
 import { View, TextInput, FlatList, Text, TouchableOpacity, ScrollView, ImageBackground } from 'react-native';
 import { Box, IconButton, Icon, Input, CloseIcon } from 'native-base';
 
-const allCountries = ['USA', 'Canada', 'Mexico', 'Brazil', 'India', 'China', 'Germany', 'France', 'Spain', 'Italy', 'Australia', 'Argentina', 'Japan', 'Russia', 'South Korea', 'Netherlands', 'United Kingdom', 'Switzerland', 'Sweden', 'Norway', 'Denmark', 'Finland', 'Belgium', 'Austria', 'Greece', 'Portugal', 'Turkey', 'Egypt', 'South Africa', 'Nigeria', 'Kenya', 'Morocco', 'Saudi Arabia', 'United Arab Emirates', 'Qatar', 'Thailand', 'Indonesia', 'Malaysia', 'Singapore', 'Vietnam', 'Philippines', 'New Zealand']; // Array of country names
+const differentSymptoms = ['Fever', 'Cough', 'Shortness of breath or difficulty breathing', 'Tiredness', 'Aches', 'Chills', 'Sore throat', 'Loss of smell', 'Loss of taste', 'Headache', 'Diarrhea', 'Severe vomiting', 'Confusion', 'Chest pain', 'Bluish lips or face']; // Array of symptoms
 
 const Search = () => {
     const [searchText, setSearchText] = useState('');
     const [suggestions, setSuggestions] = useState([]);
-    const [selectedCountries, setSelectedCountries] = useState([]);
-    const [countries, setCountries] = useState(allCountries);
+    const [selectedSymptoms, setSelectedSymptoms] = useState([]);
+    const [symptoms, setSymptoms] = useState(differentSymptoms);
 
     const handleSearch = (text) => {
         setSearchText(text);
-        const filteredCountries = countries.filter((country) =>
+        const filteredSymptoms = symptoms.filter((country) =>
             country.toLowerCase().includes(text.toLowerCase())
         );
-        filteredCountries.sort();
-        setSuggestions(filteredCountries);
+        filteredSymptoms.sort();
+        setSuggestions(filteredSymptoms);
     };
 
     const handleSelectCountry = (country) => {
-        setSelectedCountries([...selectedCountries, country]);
-        setCountries(countries.filter((item) => item !== country));
+        setSelectedSymptoms([...selectedSymptoms, country]);
+        setSymptoms(symptoms.filter((item) => item !== country));
         setSearchText('');
         setSuggestions([]);
     };
 
     const handleCountryDeletion = (country) => {
-        const newSelectedCountries = selectedCountries.filter(
+        const newSelectedSymptoms = selectedSymptoms.filter(
             (item) => item !== country
         );
-        setSelectedCountries(newSelectedCountries);
-        setCountries([...countries, country]);
+        setSelectedSymptoms(newSelectedSymptoms);
+        setSymptoms([...symptoms, country]);
     };
 
-    const renderSkillItem = ({ item }) => (
+    const renderSelectedItem = ({ item }) => (
         <Box
           key={item}
           style={{
@@ -61,32 +61,35 @@ const Search = () => {
 
     return (
         <ImageBackground source={require('../../assets/main_page.png')} style={{ flex: 1, marginBottom: 30, resizeMode: 'cover', justifyContent: 'center' }}>
-            <View style={{ position: 'absolute', top: 22, left:60, borderWidth: 1, borderColor: 'gray', borderRadius: 5, padding: 10, width: 266.7 }}>
+            <View style={{ position: 'absolute', top: 24, left:60, padding: 10, width: 266.7 }}>
                 <Input w='245'
+                    variant={'unstyled'}
                     style={{ height: 50, fontSize: 20 }}
                     placeholder="Search"
                     value={searchText}
                     onChangeText={handleSearch}
+                    focusOutlineColor={'white'}
+                    
                 />
                 <FlatList
                     data={suggestions}
-                    renderItem={({ item }) => (
+                    renderItem={({ item, index }) => (
                         searchText.length > 0 && (
-                            <TouchableOpacity onPress={() => handleSelectCountry(item)}>
-                                <Text>{item}</Text>
+                            <TouchableOpacity key={index} style={{ backgroundColor: 'white', borderColor: '#CECECE', borderWidth: 1, width: '100%', borderRadius: 5}} onPress={() => handleSelectCountry(item)}>
+                                <Text style={{ fontSize: 20, textAlign: 'left', padding: 10 }}>{item}</Text>
                             </TouchableOpacity>
                         )
                     )}
                     keyExtractor={(item) => item}
-                    style={{ maxHeight: 50 }}
+                    style={{ maxHeight: 180 }}
                 />
                 {searchText === '' && (
                     <>
                         <ScrollView style={{ maxHeight: 180}}>
                             <FlatList
                                 contentContainerStyle={{flexDirection : "row", flexWrap : "wrap"}} 
-                                data={selectedCountries}
-                                renderItem={({ item }) => renderSkillItem({ item })}
+                                data={selectedSymptoms}
+                                renderItem={({ item }) => renderSelectedItem({ item })}
                                 keyExtractor={(item) => item}
                             />
                         </ScrollView>
